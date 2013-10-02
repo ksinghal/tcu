e5b9f06697bba4c15798abe7b5cc4f7beff9d228
glasslighttech <ksinghal@thrupoint.com>
2013-10-02 15:39:12 -0400

    updated error in image creation. added authentication for uploading slider images

--- app/controllers/sliderimage_controller.rb
+++ app/controllers/sliderimage_controller.rb
@@ -1,4 +1,6 @@
 class SliderimageController < ApplicationController
+  before_filter :authenticate_admin!
+  
   def index
   end
   def new
@@ -6,8 +8,16 @@ class SliderimageController < ApplicationController
   end
   def create
     @slider_image = Sliderimage.create(params[:sliderimage])
+
+    redirect_to root_url
   end
   def edit
     @sliderimage = Sliderimage.find(params[:id])
+  end
+  def destroy
+    @sliderimage = Sliderimage.find(params[:id])
+    @sliderimage.destroy
+
+    redirect_to root_url
   end
 end
