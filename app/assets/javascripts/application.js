bc60dd42a9aa4e0807f85b152c63f6575ea35f54
glasslighttech <ksinghal@thrupoint.com>
2013-10-05 13:15:50 -0400

    hopefully fixed navbar for ios

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,16 +17,17 @@
 // = require_tree .
 
 $( document ).ready(function() {
-    if(!$("#galleria").length == 0) {
-      Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
-      Galleria.run('#galleria', {
-        autoplay: 5000,
-        transition: 'fade',
-        clicknext: true,
-        easing: 'swing',
-        showInfo: false,
-        showCounter: false,
-        thumbnails: false
-      });
-    };
+  $('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
+  if(!$("#galleria").length == 0) {
+    Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
+    Galleria.run('#galleria', {
+      autoplay: 5000,
+      transition: 'fade',
+      clicknext: true,
+      easing: 'swing',
+      showInfo: false,
+      showCounter: false,
+      thumbnails: false
+    });
+  };
 });
