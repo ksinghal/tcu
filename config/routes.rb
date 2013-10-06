ebaa2b450a42957f657a4585fce7d61625d01a17
glasslighttech <ksinghal@thrupoint.com>
2013-10-06 18:25:43 -0400

    updated to disable new user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Tcu::Application.routes.draw do
-  devise_for :admins
+  devise_for :admins, :skip => [:registrations] 
+  as :admin do
+    get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
+    put 'admins' => 'devise/registrations#update', :as => 'admin_registration'
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
