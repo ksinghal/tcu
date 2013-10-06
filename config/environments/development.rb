583dffeafbc0f8efe09f81308dbfd5c23f351bab
glasslighttech <ksinghal@thrupoint.com>
2013-10-06 15:47:49 -0400

    updated to ensure proper styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,11 +38,13 @@ Tcu::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
+  
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 end
