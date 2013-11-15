8969347c3a1f968b0fa9efb30ad9aabcc298b6f8
glasslighttech <ksinghal@thrupoint.com>
2013-11-14 23:08:11 -0500

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,7 +27,7 @@ $( document ).ready(function() {
       showInfo: false,
       showCounter: false,
       thumbnails: false,
-      height: 0.5
+      height: 0.5,
       imageCrop: 'height',
       fullscreenDoubleTap: false,
       imagePan: true,
